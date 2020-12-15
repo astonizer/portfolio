558ad2a89421bbd4fdda0b37548f5cfb5a8b6a8a
Astel <astelthottankara@gmail.com>
2020-12-15 15:02:32 +0530

    Add intro at home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,16 @@ import '../../styles/Home.css'
 function Home() {
     return (
         <div className="home">
-            
+            <div className="home_intro">
+                <div className="intro_greet">Hello, I'm</div>
+                <div className="intro_greet">Astel Thottankara</div>
+                <div className="intro_work">
+                    <p className="work-1">Full Stack Developer</p>
+                    <p className="work-2">Specialing in MERN Stack</p>
+                    <p className="work-3">Open Source Contributor</p>
+                    <p className="work-4">Competitive Programmer</p>
+                </div>
+            </div>
         </div>
     )
 }
